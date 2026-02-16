897d87b02d8a25f2e734db86b1cd63ec8cf4e25e
milay63bsm68-ops <milay63bsm68@gmail.com>
2026-02-16 03:28:15 +0100

    Update app.js

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ async function submitPayment(){
     };
 
     try{
-      const res=await fetch("https://repro-hvoc.onrender.com",{
+      const res=await fetch("https://repro-hvoc.onrender.com/send",{
         method:"POST",
         headers:{"Content-Type":"application/json"},
         body:JSON.stringify(payload)
